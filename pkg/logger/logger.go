855bd5e607648ca3c57b5b52f0b634e06a019240
Sam Mulube <sam@umbrellium.co.uk>
2018-10-09 11:06:54 +0100

    Add simple logger package with helper function for making a logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import (
+	"os"
+
+	kitlog "github.com/go-kit/kit/log"
+
+	"github.com/thingful/iotpolicystore/pkg/version"
+)
+
+// NewLogger returns an instantiated kitlog.Logger instance ready for use.
+func NewLogger() kitlog.Logger {
+	logger := kitlog.NewJSONLogger(kitlog.NewSyncWriter(os.Stdout))
+	return kitlog.With(logger, "service", version.BinaryName, "ts", kitlog.DefaultTimestampUTC)
+}
