156dfbaf63f14b62d9183f3ac84118601f26aa0d
Sam Mulube <sam@umbrellium.co.uk>
2018-10-10 17:36:04 +0100

    Add all variables in to config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,9 +3,13 @@ package config
 import kitlog "github.com/go-kit/kit/log"
 
 // Config is a shared config object we can pass around to configure components.
+// Contains attributes that the server needs in order to operate.
 type Config struct {
-	ServerAddr string
-	ConnStr    string
-	Verbose    bool
-	Logger     kitlog.Logger
+	ServerAddr         string
+	ConnStr            string
+	EncryptionPassword string
+	HashidLength       int
+	HashidSalt         string
+	Verbose            bool
+	Logger             kitlog.Logger
 }
